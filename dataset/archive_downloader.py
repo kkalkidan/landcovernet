220830286c5b3def0af19f8b86afb73cb4dabb0a
Kalkidan Fekadu <fkalkidan30@gmail.com>
2021-10-02 10:32:07 -0400

    collection id for source

--- dataset/archive_downloader.py
+++ dataset/archive_downloader.py
@@ -1,6 +1,8 @@
 
 from radiant_mlhub import client
 
-collection_id = 'ref_landcovernet_v1_labels'
+
+
+collection_id = 'ref_landcovernet_v1_source'
 
 client.download_archive(collection_id, output_dir='../data/Landcovernet_archive')
